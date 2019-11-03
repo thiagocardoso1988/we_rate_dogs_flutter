2834b5deefff7dc55450fbae7ba88f439c4dab0c
Thiago Cardoso <intelxd@gmail.com>
2019-11-03 16:30:27 -0300

    Initial commit with reusable cards

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:we_rate_dogs_app/components/dog_card.dart';
+import 'package:we_rate_dogs_app/models/dog.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'We Rate Dogs',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: MyHomePage(title: 'We Rate Dogs'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Dog> initialDoggos = []
+    ..add(new Dog('Ruby', 'Portland, OR, USA',
+        'Ruby is a very good girl. Yes: Fetch, loungin\'. No: new Dogs who get on furniture.'))
+    ..add(new Dog('Rex', 'Seattle, WA, USA', 'Best in Show 1999'))
+    ..add(new Dog('Rod Stewart', 'Prague, CZ',
+        'Star good boy on international snooze team.'))
+    ..add(new Dog('Herbert', 'Dallas, TX, USA', 'A Very Good Boy'))
+    ..add(
+        new Dog('Buddy', 'North Pole, Earth', 'Self proclaimed human lover.'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.black87,
+      ),
+      body: Container(
+        child: DogCard(initialDoggos[1]),
+      ),
+    );
+  }
+}
